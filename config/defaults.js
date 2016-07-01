a50b9959457fcf5de5dfaa314e38e35419bcd469
feizheng <1290657123@qq.com>
2016-07-01 17:18:27 +0800

    finish html &hash join

--- config/defaults.js
+++ config/defaults.js
@@ -4,9 +4,11 @@
 
   const path = require('path');
   const srcPath = path.join(__dirname, '/../src');
+  const bowerPath = path.join(__dirname, '/../bower_components');
 
   module.exports = {
     srcPath: srcPath,
+    bowerPath: bowerPath,
     publicPath: '/assets/',
     port: 8080,
     moduleLoaders: [
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@
   var defaultConfig = require('./config/defaults');
   var srcPath = defaultConfig.srcPath;
 
+  //Plugin list:
+  var ExtractTextPlugin = require('extract-text-webpack-plugin');
+  var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
   module.exports = {
     context: srcPath,
@@ -16,18 +20,35 @@
       './scripts/main'
     ],
     output: {
-      publicPath: '/dist/',
       path: './dist/',
-      filename: 'bundle.js'
+      filename: "scripts/[name]_[hash].js",
+      chunkFilename: "scripts/[id]_[hash].js"
     },
     module: {
-      loaders: defaultConfig.moduleLoaders
+      loaders: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader' // 'babel-loader' is also a legal name to reference
+        },
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!sass-loader")
+        }
+      ]
     },
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
       new webpack.NoErrorsPlugin(),
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('development')
+      }),
+      new ExtractTextPlugin('styles/[name]_[hash].css', {
+        publicPath: '/styles/'
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Fei Webpack App',
+        filename: 'index.html'
       })
     ]
   };
